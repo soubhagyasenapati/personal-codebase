5fbabcd59533875caf0935e8a7d2eccbefc11882
Soubhagya Senapati <soubhagyasenapati@gmail.com>
2017-03-16 09:43:25 -0700

    Adding new files

---
+++ sudoku/src/main/java/com/test/algo/App.java
@@ -0,0 +1,8 @@
+package com.test.algo;
+
+public class App {
+
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
+}
---
+++ sudoku/src/test/java/com/test/algo/TestApp.java
@@ -0,0 +1,14 @@
+package com.test.algo;
+
+import org.junit.Test;
+
+
+
+public class TestApp {
+	
+	@Test
+	public void doTest(){
+		System.out.println("It works :::");
+	}
+
+}
